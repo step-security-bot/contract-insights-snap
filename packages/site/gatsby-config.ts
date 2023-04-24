bcbd2c84c7af672a7f95e22bc1bbd905e641fcca
gmkung <guangmian@gmail.com>
2023-04-24 19:30:53 +0800

    Adjusted gatsby-config.ts to include /contract-insights-snap

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -4,6 +4,9 @@ const config: GatsbyConfig = {
   // This is required to make use of the React 17+ JSX transform.
   jsxRuntime: 'automatic',
 
+  // Add the pathPrefix setting
+  pathPrefix: '/contract-insights-snap',
+
   plugins: [
     'gatsby-plugin-svgr',
     'gatsby-plugin-styled-components',
